0d94e9a42e7940f38461bf7b2073e3eb9a0e8a46
Michał Kozłowski <michal@kozlovv.ski>
2022-01-11 18:57:52 +0100

    docs: wrap notebooks links in li elements

--- bin/export_notebooks.sh
+++ bin/export_notebooks.sh
@@ -5,7 +5,7 @@ for file in notebooks/*.ipynb
 do
   fileName="$(basename $file .ipynb)"
   jupyter nbconvert "notebooks/$fileName" --to html --output-dir "docs/notebooks"
-  notebooksLinks="<li>${notebooksLinks}<a href=\'./notebooks/${fileName}.html\'>${fileName}</a></li>"
+  notebooksLinks="${notebooksLinks}<li><a href=\'./notebooks/${fileName}.html\'>${fileName}</a></li>"
 done
 
 sed -i "s%NOTEBOOKS%${notebooksLinks}%" ./docs/index.html
