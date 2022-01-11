5fafd541ce31135f888c32066298999052550ecb
Michał Kozłowski <michal@kozlovv.ski>
2022-01-11 18:19:18 +0100

    ci: add notebooks index filling

--- bin/export_notebooks.sh
+++ bin/export_notebooks.sh
@@ -1,5 +1,11 @@
+cp ./templates/index.html ./docs/index.html
+
+notebooksLinks=""
 for file in notebooks/*.ipynb
 do
   fileName="$(basename $file .ipynb)"
   jupyter nbconvert "notebooks/$fileName" --to html --output-dir "docs/notebooks"
+  notebooksLinks="${notebooksLinks}<a href=\'./${fileName}.html\'>${fileName}</a>"
 done
+
+sed -i '' "s%NOTEBOOKS%${notebooksLinks}%" ./docs/index.html
